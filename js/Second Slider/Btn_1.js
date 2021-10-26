8eb1d4071ceceed227cfcb3fc419d043310a28c9
pongsoyun <seongu97@naver.com>
2021-10-26 22:59:48 +0900

    animate slider2 - 수정중

--- js/Second Slider/Btn_1.js
+++ js/Second Slider/Btn_1.js
@@ -4,6 +4,7 @@ export class Btn_1 {
     constructor(createFrame_2, createFrame_2_wrap) {
         this.num = 2;
         this.counter = 0;
+        this.delay = 1000;
 
         this.createBox = document.createElement('div');
         this.boxAttrs = this.createBox.setAttribute('class', 'Box');
@@ -22,44 +23,41 @@ export class Btn_1 {
         this.allBtn_1[1].style.right = '0px';
 
         this.width = createFrame_2.offsetWidth + 'px';
-        
-        this.allBtn_1[1].addEventListener('click', () => {
-            this.animate_2(createFrame_2);
-        });
 
         this.allBtn_1[0].addEventListener('click', () => {
-            this.animate_1(createFrame_2);
+            createFrame_2.style.flexDirection = 'row-reverse';
+
+            this.this.animation = createFrame_2.animate([
+                {transform: 'translateX(0px)'},
+                {transform: 'translateX(0px)'},
+                {transform: `translateX(${this.width})`},
+            ], {duration: this.delay});
+
+            this.remove(createFrame_2, this.animation);
+        }, false);
+
+        this.allBtn_1[1].addEventListener('click', () => {
+            createFrame_2.style.flexDirection = 'row';
+
+            this.animation = createFrame_2.animate([
+                {transform: 'translateX(0px)'}, 
+                {transform: 'translateX(0px)'},
+                {transform: `translateX(-${this.width})`},
+            ], {duration: this.delay});
+
+            this.remove(createFrame_2, this.animation);
+        }, false);
+
+        const ani = createFrame_2_wrap.getAnimations();
+        console.log(ani);
+    }
+
+    remove(createFrame_2, animation) {
+        Promise.all(
+            createFrame_2.getAnimations({ subtree: true }).map(animation => animation.finished)
+        ).then(() => {
+            this.removeItem = createFrame_2.removeChild(createFrame_2.firstChild);
+            createFrame_2.appendChild(this.removeItem);
         });
-
     }
-    animate_1(createFrame_2) {
-        createFrame_2.style.flexDirection = 'row-reverse';
-
-        this.animation = createFrame_2.animate([
-            {transform: 'translateX(0px)'}, 
-            {transform: 'translateX(0px)'},
-            {transform: `translateX(${this.width})`},
-        ], {duration: 600});
-
-        this.animation.onfinish = () => {
-            this.removeItem = createFrame_2.removeChild(createFrame_2.firstChild);
-            createFrame_2.appendChild(this.removeItem);      
-        }
-    }
-
-    animate_2(createFrame_2) {
-        createFrame_2.style.flexDirection = 'row';
-
-        this.animation = createFrame_2.animate([
-            {transform: 'translateX(0px)'}, 
-            {transform: 'translateX(0px)'},
-            {transform: `translateX(-${this.width})`},
-        ], {duration: 600});
-
-        this.animation.onfinish = () => {
-            this.removeItem = createFrame_2.removeChild(createFrame_2.firstChild);
-            createFrame_2.appendChild(this.removeItem);      
-        }
-    }
-
 }
