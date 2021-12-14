66a5c44f4c44f01eb6192de13bd317b20c0d0d5b
강선구 <seongu97@naver.com>
2021-12-14 21:58:34 +0900

    JS - 포트폴리오 애니메이션 수정/로고, 풋터 완성/메뉴,라디오 미완성

--- 포트폴리오/app.js
+++ 포트폴리오/app.js
@@ -12,67 +12,183 @@ class App {
         // add to app
         document.body.appendChild(this.app);
 
-        // create container
-        this.container = document.createElement('section');
-        this.container.setAttribute('class', 'container');
+        // create wrap
+        this.wrap = document.createElement('div');
+        this.wrap.setAttribute('class', 'wrap');
 
-        // add to container
-        this.app.appendChild(this.container);
+        // add to wrap
+        this.app.appendChild(this.wrap);
+
+        // create BG
+        this.BG = document.createElement('div');
+        this.BG.setAttribute('class', 'BG');
+
+        // add to BG
+        this.wrap.appendChild(this.BG);
+
+        // create header
+        this.header = document.createElement('header');
 
         // create logo
         this.logo = document.createElement('span');
         this.logo.setAttribute('class', 'logo');
 
-        // logo inner text
-        this.logo.innerText = 'PORTFOLIO-';
+        // title
+        const title = 'PORTFOLIO-';
+
+        // inner text in logo 
+        this.logo.innerText = title;
 
         // add to logo
-        this.container.appendChild(this.logo);
-
-        // app from to
-        gsap.fromTo('#app', 
-            {
-                x: '-500%',
-            },
-            {
-                x: '0',
-                duration: 1.5,
-            }
-        );
-
-        // container from to
-        gsap.fromTo('.container', 
-            {
-                x: '200%',
-                with: '0%',
-                height: '1%'
-            },
-            {
-                x: '0%',
-                duration: 2,
-            }
-        );
+        this.header.appendChild(this.logo);
         
-        // container to
-        gsap.to('.container', 
+        // add to header 
+        this.wrap.appendChild(this.header);
+
+        // create cntr
+        this.cntr = document.createElement('section');
+        this.cntr.setAttribute('class', 'cntr');
+
+        // add to cntr
+        this.wrap.appendChild(this.cntr);
+
+        // create footer
+        this.footer = document.createElement('footer');
+
+        // add to footer
+        this.wrap.appendChild(this.footer);
+
+        // cerate copyright
+        this.copyright = document.createElement('span');
+        this.copyright.setAttribute('class', 'copyright');
+
+        // copyright
+        const copyright = '©2021 PORTPOLIO-';
+
+        // inner text in copyright
+        this.copyright.innerText = copyright;
+
+        // add to copyright
+        this.footer.appendChild(this.copyright);
+
+        // create github
+        this.github = document.createElement('a');
+        this.github.setAttribute('class', 'github');
+        this.github.setAttribute('href', 'https://github.com/SeonGu97');
+        this.github.setAttribute('target', 'blank');
+
+        // add to github
+        this.footer.appendChild(this.github);
+
+        // github
+        const github = `<i class="fab fa-github"></i>` + 'seongu97';
+
+        // inner HTML in github
+        this.github.innerHTML = github;
+
+        // create nav
+        this.nav = document.createElement('nav');
+        
+        // add to nav
+        this.wrap.appendChild(this.nav);
+
+        // create radios
+        this.radios = document.createElement('div');
+        this.radios.setAttribute('class', 'radios');
+
+        // add to radios
+        this.wrap.appendChild(this.radios);
+
+        // create item
+        for(let i = 0; i < Setting[0].value; i++) {
+            this.item = document.createElement('input');
+            this.item.setAttribute('class', 'item');
+            this.item.setAttribute('type', 'radio');
+            this.item.setAttribute('name', 'items');
+            this.item.setAttribute('value', i);
+
+            // add to item
+            this.radios.appendChild(this.item);
+        }
+
+        // wrap
+        gsap.to('.wrap',
+            {
+                background: '#1B365C',
+                duration: 0.5,
+                delay: 1.5
+            },
+        );
+
+        // cnter
+        gsap.fromTo('.cntr',
+            {
+                width: '50%',
+                height: '0.5%',
+                borderRadius: '1rem',
+                opacity: 0.5,
+            },
+            {
+                width: '100%',
+                borderRadius: '0',
+                rotation: 0,
+                opacity: 1,
+                delay: 1,
+            }
+        );
+
+        gsap.to('.cntr',
             {
                 height: '100%',
-                delay: 2.25,
-            },
-        );
-
-        // logo from to
-        gsap.fromTo('.logo', 
-            {
-                opacity: 0,
-                y: '0',
-            },
-            {
-                opacity: 1,
-                delay: 3,
-                y: '-200%',
+                delay: 2.1
             }
         );
+
+        // header
+        gsap.from('header',
+            {
+                y: '200%',
+                delay: 3,
+                opacity: 0
+            }
+        );
+
+        // footer
+        gsap.from('footer', 
+            {
+                y: '-200%',
+                delay: 3.5,
+                opacity: 0
+            }
+        );
+
+        // nav
+        gsap.from('nav', 
+            {
+                opacity: 0,
+                delay: 4,
+            }
+        );
+
+        // radios
+        gsap.from('.radios', 
+            {
+                opacity: 0,
+                delay: 4.5,
+                stagger: 1,
+            }
+        );
+
+        // item
+        gsap.from(".item", {
+            duration: 2,
+            scale: 0.5, 
+            opacity: 0, 
+            delay: 5, 
+            stagger: 0.2,
+            ease: "elastic", 
+            force3D: true
+          });
     }
 }
 
