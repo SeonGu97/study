05ae2ed9d42328f7ae3aa0aa6378da7e202a3bc7
강선구 <seongu97@naver.com>
2021-12-29 20:47:16 +0900

    풀페이지 - 내리는 기능

--- 풀페이지/app.js
+++ 풀페이지/app.js
@@ -4,15 +4,62 @@
 const app = document.createElement('div');
 app.setAttribute('id', 'app');
 
-// create ul
-const ul = document.createElement('ul');
-ul.setAttribute('class', 'ul');
+// add to app
+document.body.appendChild(app);
+
+// create box
+const box = document.createElement('ul');
+box.setAttribute('class', 'box');
+
+// add to box
+app.appendChild(box);
 
 // value
 let value = 4;
 
-// create li
+// create item
 for(let i = 0; i < value; i++) {
-    const li = document.createElement('li');
-    li.setAttribute('class', 'li');
+    const item = document.createElement('li');
+    item.setAttribute('class', 'item');
+
+    // add to item
+    box.appendChild(item);
 }
+
+// window scroll event
+window.addEventListener('scroll', windowScroll, false);
+
+let counter = 1;
+
+let Top = document.documentElement.scrollTop || 0;
+
+// windowScroll
+function windowScroll(e) {
+    // Y
+    let Y = document.documentElement.scrollTop;
+
+    // result
+    let result = Y - Top >= 0 ? true : false;
+
+    //  Top = Y
+    Top = Y;
+
+    // gap
+    let gap = window.scrollY;
+    
+    // height
+    let height = window.innerHeight;
+
+    // style
+    document.body.style.overflow = 'hidden';
+    box.style.transition = '0.5s';
+    box.style.transform = `translateY(-${(height * counter) - gap}px)`;
+}
+
+box.addEventListener('transitionend', () => {
+    setTimeout( () => {
+        document.body.style.overflow = 'auto';
+        counter++;
+    }, 500);
+}, false);
+
