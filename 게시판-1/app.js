f310069bc1fbd10d82c8e7e9e6abad4024df9a8d
강선구 <seongu97@naver.com>
2022-02-17 22:28:03 +0900

    게시판 - 어제자 오류 고침

--- 게시판-1/app.js
+++ 게시판-1/app.js
@@ -102,115 +102,132 @@ function set_storage() {
     localStorage.setItem(name, JSON.stringify(value));
 }
 
-// text-box 클릭 이벤트 선언
-function text_box_event() {
-    text_box_.addEventListener('click', e => {
-        
-    }, false);
-}
+// num
+let num = 0;
 
-// text-box 클릭 이벤트 호출
-const text_box_event_ = text_box_event();
-
-// submit 클릭 이벤트 선언
-function submit_event() {
+// add_book 선언
+function add_book() {
     submit_.addEventListener('click', e => {
         if(!text_box_.value.trim()) {
-            // text_box_clear 호출
-            text_box_clear();
+            // clear 호출
+            clear();
         }else {
             // add_value 호출
             add_value();
 
-            // set_storage 호출
-            set_storage();
+            // create_items 호출
+            create_items();
 
-            // add_element 호출
-            add_elements();
+            // clear 호출
+            clear();
 
-            // text_box_clear 호출
-            text_box_clear();
+            // trash_event 호출
+            trash_event();
+
+            if(mod_.classList.contains('mod-change')) {
+                // remove_trash_ 호출
+                remove_trash_();
+                // add_trash 호출
+                add_trash();
+            }
         }
     }, false);
 }
 
-// submit 클릭 이벤트 호출
-const submit_event_ = submit_event();
+// add_book 호출
+const add_book_ = add_book();
 
-// text-box clear
-function text_box_clear() {
+// create_items 선언
+function create_items() {
+    const book = push('book', 'li', ['class'], ['book'], '', app.firstChild.childNodes[0].firstChild, 1);
+    const word = push('word', 'a', ['class'], ['word pointer'], text_box_.value, app.firstChild.childNodes[0].firstChild.lastChild, 1);
+}
+
+// clear 선언
+function clear() {
     text_box_.value = '';
 }
 
 // add_value 선언
 function add_value() {
     value.push(text_box_.value);
+
+    // set_storage 호출
+    set_storage();
 }
 
-// add_elements 선언
-function add_elements() {
-    const book = push('book', 'li', ['class'], ['book'], '', app.firstChild.childNodes[0].firstChild, 1);
-    const word = push('word', 'a', ['class'], ['word pointer'], text_box_.value, app.firstChild.childNodes[0].childNodes[0].lastChild, 1);
-    const list = push('list', 'div', ['class'], ['list'], value.length, app.firstChild.childNodes[1].firstChild, 1);
-
-    if(mod_.classList.contains('mod-change')) {
-        const trash = push('trash', 'span', ['class'], ['trash pointer'], '<i class="bi bi-dash-circle"></i>', app.firstChild.childNodes[0].firstChild.lastChild, 1);
-    }
-}
-
-// maintain_elements 선언
-function maintain_elements() {
+// maintain_items 선언
+function maintain_items() {
     for(let i = 0; i < value.length; i++) {
         const book = push('book', 'li', ['class'], ['book'], '', app.firstChild.childNodes[0].firstChild, 1);
-        const word = push('word', 'a', ['class'], ['word pointer'], value[i], app.firstChild.childNodes[0].childNodes[0].childNodes[i], 1);
-        const list = push('list', 'div', ['class'], ['list'], i + 1, app.firstChild.childNodes[1].firstChild, 1);
+        const word = push('word', 'a', ['class'], ['word pointer'], value[i], app.firstChild.childNodes[0].firstChild.lastChild, 1);
     }
 }
 
-// maintain_elements 호출
-const maintain_elements_ = maintain_elements();
+// maintain_items 호출
+const maintain_items_ = maintain_items();
 
-// mod_check 선언
-function mod_check() {
-    mod_.addEventListener('click', () => {
+function trash_event() {
+    for(let i = 0; i < value.length; i++) {
+        const book_ = document.querySelectorAll('.book');
+
+        book_[i].addEventListener('click', e => {
+            console.log(true);
+        }, false);
+    }
+}
+
+trash_event()
+
+// mod_change
+function mod_change() {
+    mod_.addEventListener('click', e => {
         mod_.classList.toggle('mod-change');
 
         if(mod_.classList.contains('mod-change')) {
-            mod_.classList.add('paint');
-            mod_.innerText = 'ON';
-
-            // add_trash 호출
+            add_class(mod_, 'paint');
+            change_text(mod_, 'ON');
             add_trash();
         }else {
-            mod_.classList.remove('paint');
-            mod_.innerText = 'OFF';
-
-            // remove_trash 호출
+            remove_class(mod_, 'paint');
+            change_text(mod_, 'OFF');
             remove_trash();
         }
     }, false);
 }
 
-// mod_check 호출
-const mod_check_ = mod_check();
+const mod_change_ = mod_change();
 
-// add_trash 선언
-function add_trash(book_) {
+function add_class(element, text) {
+    element.classList.add(text);
+}
+
+function remove_class(element, text) {
+    element.classList.remove(text);
+}
+
+function change_text(element, text) {
+    element.innerText = text;
+}
+
+function add_trash() {
     for(let i = 0; i < value.length; i++) {
-        const trash = push('trash', 'span', ['class'], ['trash pointer'], '<i class="bi bi-dash-circle"></i>', app.firstChild.childNodes[0].firstChild.childNodes[i], 1);
+        const trash = push('trash', 'span', ['class'], ['trash pointer'], '<i class="bi bi-dash-circle-dotted"></i>',app.firstChild.childNodes[0].firstChild.childNodes[i], 1);
     }
 }
 
-// remove_trash 선언
 function remove_trash() {
-    const book_ = document.querySelectorAll('.book');
+    mod_.classList.remove('mod-change');
 
     for(let i = 0; i < value.length; i++) {
-        book_[i].removeChild(book_[i].childNodes[1]);
+        const book_ = document.querySelectorAll('.book');
+        book_[i].childNodes[1].remove();
     }
 }
 
-// remove_book 선언
-function remove_book() {
-    
+function remove_trash_() {
+    for(let i = 0; i < value.length - 1; i++) {
+        const book_ = document.querySelectorAll('.book');
+        book_[i].childNodes[1].remove();
+    }
 }
