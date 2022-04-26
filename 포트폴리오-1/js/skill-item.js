fa39a6c697fc53e961c0e2f0f09858429566a148
강선구 <seongu97@naver.com>
2022-04-26 23:57:13 +0900

    feat: 스킬 아이콘 추가

--- 포트폴리오-1/js/skill-item.js
+++ 포트폴리오-1/js/skill-item.js
@@ -3,6 +3,16 @@
 export default class Skill_item {
     constructor(creator) {
         // create
-        creator.basket('skill_item', 'div', creator.name, 5, ['class'], ['skill_item'], '');
+        creator.basket('skill_item', 'div', creator.name, 5, ['class'], ['skill-item'], '');
+    
+        // element
+        this.Skill_item_all = document.querySelectorAll('.skill-item');
+
+        // icon array
+        this.icon = ['<i class="fa-brands fa-html5" style="color: #dd2c00"></i>', '<i class="fa-brands fa-css3-alt" style="color: #42a5f5"></i>', '<i class="fa-brands fa-js-square" style="color: #ffd600"></i>', '<i class="fa-brands fa-vuejs" style="color: #00c853"></i>', '<div class="ps">Ps</div>'];
+
+        for(let i = 0; i < this.Skill_item_all.length; i++) {
+            this.Skill_item_all[i].innerHTML = this.icon[i];
+        }
     }
 }
--- 포트폴리오-1/js/title.js
+++ 포트폴리오-1/js/title.js
@@ -3,7 +3,7 @@
 export default class Title {
     constructor(creator, main, text) {
         // create
-        creator.basket('title', 'h3', main, 1, ['class'], ['title mg-tb'], text);
+        creator.basket('title', 'h3', main, 1, ['class'], ['title basic-pd mg-tb'], text);
 
         // element
         this.icon = document.querySelectorAll('.title > i');
