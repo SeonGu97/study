1e384cfa5501fd57e8293af07b12d757455566ce
강선구 <seongu97@naver.com>
2022-06-09 16:15:35 +0900

    docs: 파라미터 변경

--- side/JS/clock/clock.js
+++ side/JS/clock/clock.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export default class Clock {
-    constructor(Create, size) {
-        const _clock = new Create('clock', 'div', '', '', _app);
+    constructor(Create, _nav) {
+        const _clock = new Create('clock', 'div', 'class', 'clock basics hide', _nav);
     }
 }
