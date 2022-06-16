2410398705b7e1e723b9856808df85b7ce2db17e
강선구 <seongu97@naver.com>
2022-06-16 23:11:12 +0900

    feat: sns 추가

---
+++ side/JS/aside/sns.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class SNS {
+    constructor(Create, _aside) {
+        const _sns = new Create('sns', 'div', 'class', 'sns basics hide-s-y un-transparent', _aside);
+    }
+}
