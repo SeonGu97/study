f50d44d9fb255004e9ceedf21885ef3892360786
강선구 <seongu97@naver.com>
2022-06-18 00:28:37 +0900

    feat: add active effect

---
+++ side/JS/media/active.js
@@ -0,0 +1,8 @@
+'ust strict';
+
+export default class Active {
+    constructor(Create, _size) {
+        // create
+        const _active = new Create('active', 'div', 'class', 'active', _size);
+    }
+}
--- side/JS/media/size.js
+++ side/JS/media/size.js
@@ -1,5 +1,6 @@
 'ust strict';
 
+import Active from "./active.js";
 import Device from "./device.js";
 
 export default class Size {
@@ -9,5 +10,22 @@ export default class Size {
 
         // component
         const device = new Device(Create, _size);
+        const active = new Active(Create, _size);
+
+        // event
+        _size.addEventListener('click', this.event, false);
+    }
+
+    event(e) {
+        const target = e.target;
+
+        let x = target.getBoundingClientRect().left;
+
+        let size = target.parentElement.getBoundingClientRect().width;
+        let half = target.parentElement.getBoundingClientRect().width / 2;
+        let windowX = (window.innerWidth / 2) - (size + half);
+        
+        const active = document.querySelector('.active');
+        active.style.left = `${x - size - windowX}px`;
     }
 }
