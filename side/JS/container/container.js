3fe054175f68cddf133c7162e74ddcbf9fb1dda2
강선구 <seongu97@naver.com>
2022-06-18 21:24:32 +0900

    feat: add screen

--- side/JS/container/container.js
+++ side/JS/container/container.js
@@ -1,8 +1,13 @@
 'ust strict';
 
+import Screen from "./screen.js";
+
 export default class Container {
     constructor(Create, _app) {
         // create
-        const _container = new Create('container', 'section', 'class', 'container basics', _app);
+        const _container = new Create('container', 'section', 'class', 'container desktop', _app);
+    
+        // component
+        const screen = new Screen(Create, _container);
     }
 }
---
+++ side/JS/container/screen.js
@@ -0,0 +1,8 @@
+'ust strict';
+
+export default class Screen {
+    constructor(Create, _container) {
+        // create
+        const _screen = new Create('screen', 'div', 'class', 'screen', _container);
+    }
+}
