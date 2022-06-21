da67827b0a88e018dad517097bd216e5316321ad
강선구 <seongu97@naver.com>
2022-06-21 16:14:57 +0900

    feat: add Screen

--- _Portfolio/JS/app/app.js
+++ _Portfolio/JS/app/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
 import Create from "../create/create.js";
+import Screen from "../screen/screen.js";
 
 export default class App {
     constructor() {
         const _app = new Create('app', 'div', 'id', 'app', document.body);
+
+        const _screen = new Screen(Create, _app);
     }
 }
 
---
+++ _Portfolio/JS/screen/screen.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class Screen {
+    constructor(Create, _app) {
+        const _screen = new Create('screen', 'div', 'class', 'screen', _app);
+    }
+}
