b0c3b9dc35225f6647bab9afa2b15cd8a93798c6
강선구 <seongu97@naver.com>
2022-06-21 18:20:00 +0900

    feat: add Device

---
+++ _Portfolio/JS/header/device.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class Device {
+    constructor(Create, _header) {
+        const _device = new Create('device', 'div', 'class', 'device', _header);
+    }
+}
--- _Portfolio/JS/header/header.js
+++ _Portfolio/JS/header/header.js
@@ -1,7 +1,11 @@
 'use strict';
 
+import Device from "./device.js";
+
 export default class Header {
     constructor(Create, _screen) {
         const _header = new Create('header', 'header', '', '', _screen);
+
+        const _device = new Device(Create, _header);
     }
 }
