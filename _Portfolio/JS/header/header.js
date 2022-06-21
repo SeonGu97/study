43c37e8ae0c742c8ecd7d48fc9e154245bfaaa7e
강선구 <seongu97@naver.com>
2022-06-21 17:07:44 +0900

    feat: add Header

---
+++ _Portfolio/JS/header/header.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class Header {
+    constructor(Create, _screen) {
+        const _header = new Create('header', 'header', '', '', _screen);
+    }
+}
--- _Portfolio/JS/screen/screen.js
+++ _Portfolio/JS/screen/screen.js
@@ -1,7 +1,11 @@
 'use strict';
 
+import Header from "../header/header.js";
+
 export default class Screen {
     constructor(Create, _app) {
         const _screen = new Create('screen', 'div', 'class', 'screen', _app);
+
+        const _header = new Header(Create, _screen);
     }
 }
