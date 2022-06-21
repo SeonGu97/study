5546abd1eb2dfe583ccf122e32d2787d42204b8c
강선구 <seongu97@naver.com>
2022-06-21 18:40:22 +0900

    feat: Device-icons in add an icons inside

--- _Portfolio/JS/header/device-icons.js
+++ _Portfolio/JS/header/device-icons.js
@@ -4,10 +4,12 @@ export default class Device_icons {
     constructor(Create, _device) {
         const num = 3;
 
-        const icons = [];
+        const icons = ['<i class="fa-solid fa-display"></i>', '<i class="fa-solid fa-tablet-screen-button"></i>', '<i class="fa-solid fa-mobile-screen-button"></i>'];
 
         for(let i = 0; i < num; i++) {
             const _device_icons = new Create('device_icons', 'span', 'class', 'device-icons', _device);
+
+            _device_icons.innerHTML = icons[i];
         }
     }
 }
