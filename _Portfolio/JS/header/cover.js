7ddf5cfc3873de5a64ab7db39c694f6aa88cbee6
강선구 <seongu97@naver.com>
2022-06-23 18:47:21 +0900

    feat: add Cover

---
+++ _Portfolio/JS/header/cover.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class Cover {
+    constructor(Create, _device) {
+        const _cover = new Create('cover', 'div', 'class', 'cover', _device);
+    }
+}
--- _Portfolio/JS/header/device.js
+++ _Portfolio/JS/header/device.js
@@ -1,12 +1,14 @@
 'use strict';
 
 import Device_icons from "./device-icons.js";
+import Cover from "./cover.js";
 
 export default class Device {
     constructor(Create, _header) {
         const _device = new Create('device', 'div', 'class', 'device', _header);
 
         const device_icons = new Device_icons(Create, _device);
+        const cover = new Cover(Create, _device);
 
         _device.firstChild.classList.add('paint');
     }
