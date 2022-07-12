fa0ef67e3981982b5a83c43a2e9f94cfd3fcf7dc
강선구 <seongu97@naver.com>
2022-07-12 19:01:16 +0900

    feat: clock switch 완성

--- _Portfolio/JS/clock/clock.js
+++ _Portfolio/JS/clock/clock.js
@@ -1,15 +1,15 @@
 'use strict';
 
+import Digital from "./digital.js";
 import Analog from "./analog.js"
 import Clock_switch from "./clock_switch.js";
-import Digital from "./digital.js";
 
 export default class Clock {
     constructor(Create, aside) {
         const clock = new Create('clock', 'div', 'class', 'clock', aside);
 
-        const analog = new Analog(Create, clock);
         const digital = new Digital(Create, clock);
+        const analog = new Analog(Create, clock);
         const clock_switch = new Clock_switch(Create, clock);
     }
 }
--- _Portfolio/JS/clock/clock_switch.js
+++ _Portfolio/JS/clock/clock_switch.js
@@ -7,9 +7,42 @@ export default class Clock_switch {
         for(let i = 0; i < 2; i++) {
             const _switch = new Create('_switch', 'div', 'class', 'switch', clock_switch);
         
-            const array = ['']
+            const array = ['type1', 'type2'];
+
+            _switch.innerText = array[i];
         }
 
         const cover = new Create('cover', 'div', 'class', 'cover', clock_switch);
+
+        const _switch = document.querySelectorAll('.switch');
+        _switch[0].classList.add('paint');
+
+        _switch.forEach((element, index) => {
+            element.addEventListener('click', e => {
+                const clock = document.querySelector('.clock');
+
+                for(let i = 0; i < clock.childNodes.length; i++) {
+                    clock.childNodes[i].classList.remove('hide');
+                }
+
+                clock.childNodes[index].classList.add('hide');
+
+                const width = cover.getBoundingClientRect().width;
+
+                if(index == 0) {
+                    cover.style.left = `${(width * index) + 4}px`;
+                }else {
+                    cover.style.left = `${(width * index) + 12}px`;
+                }
+
+                let target = e.target;
+
+                for(let i = 0; i < _switch.length; i++) {
+                    _switch[i].classList.remove('paint');
+                }
+
+                target.classList.add('paint');
+            }, false);
+        });
     }
 }
